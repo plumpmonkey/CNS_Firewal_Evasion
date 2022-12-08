f84707937a0b4ac2a7c0d41877ad4677c7f6b1c1
Jon White <white.jon@gmail.com>
2022-12-08 17:21:56 +0000

    Port Hopping Example Complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# CNS Firewall Evasion Lab
+
+A lab environment to support the UWE UFCFVN-30-M Computer and Network Security Firewall Evasion Lab. Refer to the lab worksheet for more information.
--- www_node/views/js/script.js
+++ www_node/views/js/script.js
@@ -1,11 +1,11 @@
 const msgEl = document.querySelector('#message')
 
 function submitForm() {
-    const userNameInput = document.querySelector('#name').value;
-    if(userNameInput.length == 0) {
+    const commandInput = document.querySelector('#name').value;
+    if(commandInput.length == 0) {
         msgEl.innerHTML = 'The form needs a command!!'
     } else {
-        $.post('/form-submission', {name: userNameInput}, function(response) {
+        $.post('/form-submission', {name: commandInput}, function(response) {
             console.log(response)
             msgEl.innerHTML = 'Command submitted!'
         })
